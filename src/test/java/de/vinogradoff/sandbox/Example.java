9292421ac1f4d78fd1536e165c4359bb12ee4d93
Alexei Vinogradov <alexei@vinogradoff.de>
2018-10-30 16:18:21 +0100

    initial

---
+++ src/test/java/de/vinogradoff/sandbox/Example.java
@@ -0,0 +1,13 @@
+package de.vinogradoff.sandbox;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Example {
+  @Test
+  public void simpleTest() {
+    assertThat(true).isTrue();
+  }
+}
